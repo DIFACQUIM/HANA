22c87286c84f0b3b4446463f3f4d0849d53c5a29
Ana Luisa Chávez <58413133+AnaChavezHdz@users.noreply.github.com>
2025-01-08 05:23:28 -0600

    Update hana.py

--- hana.py
+++ hana.py
@@ -7,10 +7,7 @@ from rdkit import Chem
 import matplotlib.pyplot as plt
 
 SMARTS_df = pd.read_csv("https://raw.githubusercontent.com/DIFACQUIM/HANA/refs/heads/main/SMARTS_list.csv")
-print(SMARTS_df.tail(2))
-
 SMARTS_list = list(SMARTS_df["SMARTS"])
-print(SMARTS_list[:2])
 
 def HANA (smi): #HANA molecular fingerprint
   bits_list = [] # List of bits
