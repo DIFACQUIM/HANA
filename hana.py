a7cc5d72c040ed6bfa7c823533c490109e880b33
Ana Luisa Chávez <58413133+AnaChavezHdz@users.noreply.github.com>
2025-02-24 21:05:07 -0600

    Update hana.py

--- hana.py
+++ hana.py
@@ -4,22 +4,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-SMARTS_1 = pd.read_csv("https://raw.githubusercontent.com/DIFACQUIM/HANA/refs/heads/main/SMARTS/HANA_75.csv")
-SMARTS_2 = pd.read_csv("https://raw.githubusercontent.com/DIFACQUIM/HANA/refs/heads/main/SMARTS/HANA_1363.csv")
-SMARTS_list1 = list(SMARTS_1["SMARTS"])
-SMARTS_list2 = list(SMARTS_2["SMARTS"])
+SMARTS_df = pd.read_csv("https://raw.githubusercontent.com/DIFACQUIM/HANA/refs/heads/main/SMARTS/HANA_1363.csv")
+SMARTS_list = list(SMARTS_df["SMARTS"])
 
 def hana (mol,bits): #HANA 
   bits_list = [] # List of bits
 
   # Bits
-  if bits==75:
-    SMARTS_list = SMARTS_list1
+  SMARTS_bits = SMARTS_list[:bits]
     
-  elif bits==1363:
-    SMARTS_list = SMARTS_list2
-    
-  for i in SMARTS_list:
+  for i in SMARTS_bits:
     if mol.HasSubstructMatch(Chem.MolFromSmarts(i)):
       bits_list.append(1)
     else:
